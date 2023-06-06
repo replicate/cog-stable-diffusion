3682fcd5ad389896a0c7cd85071d2590d94b1fee
Dan <dan.nelson8@gmail.com>
2023-06-06 22:55:43 +0000

    fixed safety checker loading

--- load_time_tests/prepare_model_files.py
+++ load_time_tests/prepare_model_files.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import glob
 import os
 import shutil
 import sys
-import torch
 
+import torch
 from diffusers import StableDiffusionPipeline
 from diffusers.pipelines.stable_diffusion.safety_checker import \
     StableDiffusionSafetyChecker
@@ -13,7 +14,7 @@ MODEL_ID = "stabilityai/stable-diffusion-2-1"
 MODEL_CACHE = "diffusers-cache"
 SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
-saftey_checker = StableDiffusionSafetyChecker.from_pretrained(
+safety_checker = StableDiffusionSafetyChecker.from_pretrained(
     SAFETY_MODEL_ID,
     cache_dir=MODEL_CACHE,
     torch_dtype=torch.float16
@@ -21,38 +22,32 @@ saftey_checker = StableDiffusionSafetyChecker.from_pretrained(
 
 pipe = StableDiffusionPipeline.from_pretrained(
     MODEL_ID,
-    saftey_checker=saftey_checker,
+    safety_checker=safety_checker,
     cache_dir=MODEL_CACHE,
     torch_dtype=torch.float16
 )
 
 # # Save fp16 .bin weights
 print('Saving fp16 .bin weights')
-saftey_checker.save_pretrained('weights/fp16/bin/safety_checker/')
+safety_checker.save_pretrained('weights/fp16/bin/safety_checker/')
 pipe.save_pretrained('weights/fp16/bin/')
 
 print('Saving fp16 .safetensors weights')
 # Save fp16 .safetensor weights
-saftey_checker.save_pretrained('weights/fp16/safetensors/safety_checker', safe_serialization=True)
+safety_checker.save_pretrained('weights/fp16/safetensors/safety_checker', safe_serialization=True)
 pipe.save_pretrained('weights/fp16/safetensors/', safe_serialization=True)
 
-
-## Tensorize models
-from tensorizer import TensorSerializer
-import json 
-
-def ignore_bin_files(dirname, filenames):
-    return [name for name in filenames if name.endswith('.bin')]
-
+# save fp16 tensorized weights
 tensorized_weights_base_path = "weights/fp16/tensors/"
 if os.path.exists(tensorized_weights_base_path):
     shutil.rmtree(tensorized_weights_base_path)
 
-shutil.copytree('weights/fp16/bin/', tensorized_weights_base_path)#, ignore=ignore_bin_files)
+shutil.copytree('weights/fp16/bin/', tensorized_weights_base_path)
 
 component_map = {}
 for k, component in pipe.components.items():
     if isinstance(component, torch.nn.Module):
+        print(f'tensorizing {k}')
         path = os.path.join(tensorized_weights_base_path, k, f"{k}.tensors")
         serializer = TensorSerializer(path)
         serializer.write_module(component)
@@ -63,6 +58,10 @@ for k, component in pipe.components.items():
             'cls': component.__class__.__name__,
             'module': component.__module__,
             }
+
+# remove .bin from tensors file
+for torch_binary in glob.glob('weights/fp16/tensors/**/*.bin'):
+    os.remove(torch_binary)
         
 
 # # Save fp16 .bin weights
